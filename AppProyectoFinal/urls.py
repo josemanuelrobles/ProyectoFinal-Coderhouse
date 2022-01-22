b35fd618232235ab1b7d5e9a7b4c6527d42c5336
José Robles <jm_12_95@hotmail.com>
2022-01-22 20:16:56 -0300

    template corriendo

--- AppProyectoFinal/urls.py
+++ AppProyectoFinal/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from AppProyectoFinal import views
 
 
+
 urlpatterns = [
     path("" ,views.inicio ),
     
--- AppProyectoFinal/views.py
+++ AppProyectoFinal/views.py
@@ -1,7 +1,20 @@
 from xml.dom import HierarchyRequestErr
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
 from django.http import HttpResponse 
-def inicio (request):
-    return HttpResponse ("Página de Inicio")
+from django.template import Context,Template
+
+
+
+# class Inicio (TemplateView):
+#     template_name= "Inicio.html"
+
+def inicio(self):
+    miHtml=open("C:/Users/jm_12/OneDrive/Escritorio/Proyecto Entrega Final/AppProyectoFinal/templates/AppProyectoFinal/Inicio.html")
+    plantilla = Template(miHtml.read())
+    miHtml.close()
+    miContexto = Context()
+    documento = plantilla.render(miContexto)
+    return HttpResponse(documento)
+    
